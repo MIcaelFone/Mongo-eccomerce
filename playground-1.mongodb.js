d2d7d718d232a3bcb56c25e59ce2240d0bdec7e7
Micael Fone <micaelfone0@gmail.com>
2024-11-12 20:22:35 -0300

    Estruturando os playsgrounds

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -177,4 +177,25 @@ db.getCollection('products').insertMany([
 console.log("Produtos inseridos com sucesso");
  
 
- 
+db.getCollection('transacoes').insertOne({
+    idproduto: ObjectId("67328153a45cda7afe4cd0fe"),
+    idusuario: ObjectId("67328153a45cda7afe4cd0f5"),
+    quantidade: 2,
+    valor_total: 200
+});
+
+console.log("Transação inserida com sucesso");
+db.getCollection("avaliacoes").insertMany([
+    {
+        idproduto: ObjectId("67328153a45cda7afe4cd0ff"),
+        idtransacao: ObjectId('673105ed366b0e83553d4822'),
+        idusuario: ObjectId("67328153a45cda7afe4cd0f4"),
+        nota: Int32(5)
+    },
+    {
+        idproduto: ObjectId("67328153a45cda7afe4cd0fe"),
+        idtransacao: ObjectId('673105ed366b0e83553d4822'),
+        idusuario: ObjectId("67328153a45cda7afe4cd0f5"),
+        nota: Int32(20)
+    }
+]);
