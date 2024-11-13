fae65f443eef640231803b1def71557f03a7b357
Micael Fone <micaelfone0@gmail.com>
2024-11-13 16:10:36 -0300

    Mudanças no playground 6

--- playground-6.mongodb.js
+++ playground-6.mongodb.js
@@ -1,11 +1,59 @@
-db.usuarios.updateMany(
-    { "localizacao": { $exists: false } }, // Filtra usuários sem campo de localização
-    { $set: { 
-        "localizacao": { 
-            type: "Point", 
-            coordinates: [-49.2767, -25.4284]  // Valor de exemplo para Curitiba, altere conforme necessário
-        } 
-    }}
- );
+//Use o comando abaixo para executar o arquivo
+db.users.updateMany(
+    {},
+    { $set: { location: { type: "Point", coordinates: [longitude, latitude] } } }
+);
+//Criar índices 2dsphere
+db.users.createIndex({ location: "2dsphere" });
+db.products.createIndex({ location: "2dsphere" });
+//Consulta de distancia    
+db.products.find({
+    location: {
+      $near: {
+        $geometry: { type: "Point", coordinates: [userLongitude, userLatitude] },
+        $maxDistance: raioEmMetros
+      }
+    }
+});
+//Agregação para Média de Distância entre Compradores e Vendedores:
+db.transactions.aggregate([
+    {
+      $lookup: {
+        from: "products",
+        localField: "idProduto",
+        foreignField: "_id",
+        as: "produto"
+      }
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "userId",
+        foreignField: "_id",
+        as: "usuario"
+      }
+    },
+    {
+      $addFields: {
+        distancia: {
+          $geoNear: {
+            $geometry: { type: "Point", coordinates: ["$usuario.location", "$produto.location"] },
+            spherical: true
+          }
+        }
+      }
+    },
+    { $group: { _id: null, distanciaMedia: { $avg: "$distancia" } } }
+]);
+// Buscar Produtos por Proximidade Geográfica:
+db.products.find({
+    location: {
+      $near: {
+        $geometry: { type: "Point", coordinates: [userLongitude, userLatitude] },
+        $maxDistance: raioEmMetros
+      }
+    }
+});
+  
  
  
